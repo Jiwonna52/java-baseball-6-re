5382f66888ff624e2c328b6be131c5a16bc16819
Jiwon Na <jiwonna52@gmail.com>
2023-11-25 22:43:06 +0900

    Feat : 스트라이크 된 숫자를 표시한 후 표시된 것들만 세주는 기능 추가

--- src/main/java/baseball/service/BaseballService.java
+++ src/main/java/baseball/service/BaseballService.java
@@ -5,14 +5,26 @@ import java.util.Objects;
 
 public class BaseballService {
     private static int length = 3;
-    public int checkStrike(List<Integer> userNumber, List<Integer> computerNumber) {
-        int count = 0;
+    public boolean[] checkStrike(List<Integer> userNumber, List<Integer> computerNumber) {
+        boolean[] visitNumber = new boolean[length];
         for(int i=0; i<length; i++) {
             if (Objects.equals(userNumber.get(i), computerNumber.get(i))) {
+                visitNumber[i] = true;
+            }
+        }
+        return visitNumber;
+    }
+
+    public int countStrike(boolean[] visitNumber) {
+        int count = 0;
+        for (boolean visit : visitNumber) {
+            if (visit) {
                 count += 1;
             }
         }
         return count;
     }
+
+
 
 }
--- src/test/java/baseball/serviceTest/BaseballServiceTest.java
+++ src/test/java/baseball/serviceTest/BaseballServiceTest.java
@@ -5,6 +5,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 
@@ -12,28 +13,35 @@ public class BaseballServiceTest {
 
     private BaseballService baseballService = new BaseballService();
 
+
     @DisplayName("스트라이크 확인 테스트")
     @Test
     void checkStrikeTest() {
         List<Integer> userNumber1 = new LinkedList<>(List.of(1, 2, 3));
         List<Integer> computerNumber1 = new LinkedList<>(List.of(1, 2, 3));
-        Assertions.assertThat(baseballService.checkStrike(userNumber1, computerNumber1))
-                .isEqualTo(3);
+        boolean[] visitNumber1 = baseballService.checkStrike(userNumber1, computerNumber1);
+        int count1 = baseballService.countStrike(visitNumber1);
+        Assertions.assertThat(count1).isEqualTo(3);
+
 
         List<Integer> userNumber2 = new LinkedList<>(List.of(1, 2, 4));
         List<Integer> computerNumber2 = new LinkedList<>(List.of(1, 2, 3));
-        Assertions.assertThat(baseballService.checkStrike(userNumber2, computerNumber2))
-                .isEqualTo(2);
+        boolean[] visitNumber2 = baseballService.checkStrike(userNumber2, computerNumber2);
+        int count2 = baseballService.countStrike(visitNumber2);
+        Assertions.assertThat(count2).isEqualTo(2);
 
         List<Integer> userNumber3 = new LinkedList<>(List.of(1, 4, 5));
         List<Integer> computerNumber3 = new LinkedList<>(List.of(1, 2, 3));
-        Assertions.assertThat(baseballService.checkStrike(userNumber3, computerNumber3))
-                .isEqualTo(1);
+        boolean[] visitNumber3 = baseballService.checkStrike(userNumber3, computerNumber3);
+        int count3 = baseballService.countStrike(visitNumber3);
+        Assertions.assertThat(count3).isEqualTo(1);
+
 
         List<Integer> userNumber4 = new LinkedList<>(List.of(4, 5, 6));
         List<Integer> computerNumber4 = new LinkedList<>(List.of(1, 2, 3));
-        Assertions.assertThat(baseballService.checkStrike(userNumber4, computerNumber4))
-                .isEqualTo(0);
+        boolean[] visitNumber4 = baseballService.checkStrike(userNumber4, computerNumber4);
+        int count4 = baseballService.countStrike(visitNumber4);
+        Assertions.assertThat(count4).isEqualTo(0);
     }
 
 }
