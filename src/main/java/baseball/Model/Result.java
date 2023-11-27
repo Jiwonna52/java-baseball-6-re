4c3d768565754a15a6f76cc64718a7b3a9618a60
Jiwon Na <jiwonna52@gmail.com>
2023-11-27 22:50:18 +0900

    Feat : 결과 담는 nodel 추가

---
+++ src/main/java/baseball/Model/Result.java
@@ -0,0 +1,11 @@
+package baseball.Model;
+
+public class Result {
+    private int strikeNumber;
+    private int ballNumber;
+
+    public Result(int strikeNumber, int ballNumber) {
+        this.strikeNumber = strikeNumber;
+        this.ballNumber = ballNumber;
+    }
+}
