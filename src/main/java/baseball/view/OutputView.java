22e3afb90dd0c433e4ba724388f51ae2e1950a68
Jiwon Na <jiwonna52@gmail.com>
2023-11-27 23:00:06 +0900

    Feat : 출력 메시지 기능 추가

---
+++ src/main/java/baseball/view/OutputView.java
@@ -0,0 +1,35 @@
+package baseball.view;
+
+import baseball.Model.Result;
+
+import java.util.List;
+
+public class OutputView {
+
+    public void printWStartGame() {
+        System.out.println("숫자 야구 게임을 시작합니다.");
+    }
+
+    public void printGetUserNumber() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+    public void printBallResult(int ballCount) {
+        System.out.print(ballCount + "볼");
+    }
+
+    public void printStrikeResult(int strikeCount) {
+        System.out.println(strikeCount + "스트라이크");
+    }
+
+    public void printNothingResult() {
+        System.out.println("나싱");
+    }
+
+    public void printWin() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
+
+    public void printGoOrStop() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+}
