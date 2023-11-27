28f20c4aae85ccc561f701a17d5e3f39c24d7c60
Jiwon Na <jiwonna52@gmail.com>
2023-11-27 22:21:54 +0900

    Feat : 숫자 입력 기능 추가

---
+++ src/main/java/baseball/service/InputService.java
@@ -0,0 +1,14 @@
+package baseball.service;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class InputService {
+    private BufferedReader br;
+    public String inputNumber() throws IOException {
+        br = new BufferedReader(new InputStreamReader(System.in));
+        return br.readLine();
+    }
+    
+}
